19266a64545476ffa4d2ba80ea83c875f7f85f51
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-06 19:15:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
 count = 0 
-until array[count] = value_to_find
+if array[count] != value_to_find
   count += 1 
-  
+
+end
 
 def find_max_value(array)
   new_array = array.sort 
